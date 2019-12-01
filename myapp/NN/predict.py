a55d765cf53590a1f974dd95bbd9a64f1c20c968
Daria Prudilnik <Daria.Prudilnik@de.ibm.com>
2019-12-01 16:40:30 +0300

    added new NN

--- myapp/NN/predict.py
+++ myapp/NN/predict.py
@@ -2,69 +2,105 @@ from keras.models import model_from_json
 import pyexcel as pe
 import numpy
 from sklearn.preprocessing import LabelBinarizer
-import keras.losses as losses
+# from collections import Counter
+# import keras.losses as losses
+from train import Train
+
 
 class Predict():
-    def __init__(self, dataset=pe.get_array(file_name='dataset.xls')):
-
+    def __init__(self, dataset=pe.get_array(file_name='test_dataset.xls')):
         self.excel_dataset = pe.get_array(file_name='dataset.xls')
         self.dataset = dataset
         self.classes = []
+        self.set_list = []
+        self.numb_arr = []
+        self.x = []
 
     def predict_data(self):
 
-        new_list = []
-        for i in self.dataset:
-            temp = []
-            for j in i:
-                byt = j.encode('utf-8')
-                to_int = int.from_bytes(byt, "big")
-                to_float = float.fromhex(hex(to_int))
-                temp.append(to_float)
-            new_list.append(temp)
+        print(self.dataset)
+        t = Train()
+        t.set_x()
+        self.set_list = t.set_list
+        numb_arr = []
+        length = len(self.dataset[0])
+        # print(len(self.dataset))
+        # print(length)
 
-        arr = numpy.asarray(new_list)
+        arr = numpy.asarray(self.dataset)
+        length = len(arr[0])
+        new_arr = []
+        # swapp arr
+        for column in range(length):
+            temp_arr = []
+            for row in range(len(arr)):
+                temp_arr.append(self.dataset[row][column])
+            new_arr.append(temp_arr)
 
-        x = arr[:, 0:11]
-        xarr = x - x.mean()
-        xarr = xarr / xarr.max()
+        new_arr = numpy.asarray(new_arr)
+        # print(new_arr)
+
+        for i in range(length):  # col - 11
+            temp_list = []
+            for j in range(len(self.dataset)):  # rows - 1
+                for counter in range(len(self.set_list[i])): # - 12 5
+                    if new_arr[i][j] == self.set_list[i][counter]:
+                        temp_list.append(counter)
+                    else:
+                        ...
+
+            self.numb_arr.append(temp_list)
+
+        self.x = numpy.asarray(self.numb_arr)
+        self.x = numpy.reshape(self.x, (self.x.shape[1], self.x.shape[0]))
+
+
+        encoder = LabelBinarizer()
 
         y_arr = numpy.asarray(self.excel_dataset)
         y = y_arr[:, 11]
-        encoder = LabelBinarizer()
-        y_categorial = encoder.fit_transform(y)
 
+        encoder.fit_transform(y)
         json_file = open('model.json', 'r')
         loaded_model_json = json_file.read()
         json_file.close()
-
         loaded_model = model_from_json(loaded_model_json)
         loaded_model.load_weights("model.h5")
+        loaded_model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-
-        # score = loaded_model.evaluate(xarr, y_categorial, verbose=0)
-        # print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1] * 100))
-
-        predictions = loaded_model.predict(xarr)
-        predictions_class = loaded_model.predict_classes(xarr)
-
+        # predictions = loaded_model.predict(self.x)
+        predictions_class = loaded_model.predict_classes(self.x)
         self.classes = encoder.classes_
 
         # print(self.classes)
         # print(' {} => class - {} name {} (expected {})'.format(predictions, predictions_class, encoder.classes_[predictions_class], y_categorial))
         # print("Prediction: {}".format(encoder.classes_[predictions_class][0]))
 
-
-        return encoder.classes_[predictions_class][0]
-
         # for i in range(5):
         #     print(' {} => class - {} name {} (expected {})'.format(predictions[i], predictions_class[i], encoder.classes_[predictions_class[i]],  y_categorial[i]))
 
 
-p = Predict([['нейтральные комфортные дни', 'не уверен, удивите меня', 'парой', 'всего понемногу',
+        return encoder.classes_[predictions_class][0]
+
+
+p0 = Predict([['нейтральные комфортные дни', 'не уверен, удивите меня', 'парой', 'всего понемногу',
               'необычные местные блюда', 'не имеет значения', 'командные, игровые', 'места с историей',
               'отдых от работы и людей', 'две недели', 'в туристической части города']])
+a0 = p0.predict_data()
+print(a0)
 
+
+
+p = Predict([['настоящая зима и много снега', 'ленивый', 'парой', 'спорт', 'необычные местные блюда',
+              'несколько ( до 6) часов', 'командные, игровые', 'причудливые и максимально необычные', 'отдых от работы и людей', 'на неделю',
+              'нет предпочтений']])
 a = p.predict_data()
 print(a)
+
+
+p1 = Predict([['нейтральные комфортные дни', 'не уверен, удивите меня', 'парой', 'всего понемногу',
+               'необычные местные блюда', 'не имеет значения', 'командные, игровые', 'места с историей',
+               'отдых от работы и людей', 'на уикэнд', 'в туристической части города']])
+a1 = p1.predict_data()
+print(a1)
+
--- myapp/NN/train.py
+++ myapp/NN/train.py
@@ -1,53 +1,107 @@
 import pyexcel as pe
 import numpy
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import LabelBinarizer, StandardScaler
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Activation, Flatten, Dropout
+from keras.preprocessing.text import one_hot, text_to_word_sequence
+from collections import Counter
+
+class Train:
+    def __init__(self, dataset = pe.get_array(file_name='dataset.xls')):
+        self.dataset = dataset
+        self.model = Sequential()
+        self.x = []
+        self.y_categorical = []
+        self.numb_arr = []
+        self.set_list = []
+
+    def set_x(self):
+
+        arr = numpy.asarray(self.dataset)
+        length = len(arr[0])
+        new_arr = []
+        #swapp arr
+        for column in range(length):
+            temp_arr = []
+            for row in range(len(arr)):
+                temp_arr.append(self.dataset[row][column])
+            new_arr.append(temp_arr)
+
+        new_arr = numpy.asarray(new_arr)
+
+
+        unique_list = []
+
+        for counter in range(len(new_arr)):
+            unique_list.append(len(Counter(new_arr[counter]).keys()))
+
+
+        self.set_list = []  # list of indexes: unique values
+        for elem in new_arr:
+            data_list = [key for key in Counter(elem)]
+            temp_dict = {i: data_list[i] for i in range(0, len(data_list))}
+            self.set_list.append(temp_dict)
+
+        # print(self.set_list)
+
+        self.numb_arr = []
+
+        for i in range(length): #col
+            temp_list = []
+            for j in range(len(self.dataset)): #rows
+                for counter in range(len(self.set_list[i])):
+                    if new_arr[i][j] == self.set_list[i][counter]:
+                        temp_list.append(counter)
+                    else:
+                        ...
+
+            self.numb_arr.append(temp_list)
+
+        #set_x
+        x = numpy.asarray(self.numb_arr)
+        x = x[0:11]
+        self.x = numpy.reshape(x, (x.shape[1], x.shape[0]))
+
+
+    def set_y(self):
+
+        y_arr = numpy.asarray(self.dataset)
+        y = y_arr[:, 11]
+        encoder = LabelBinarizer()
+        self.y_categorial = encoder.fit_transform(y)
+
+    def train(self):
+
+        self.model.add(Dense(11, input_shape=(11, ), activation='relu'))
+        self.model.add(Dense(200, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(100, activation='relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(self.y_categorial.shape[1], activation='softmax'))
+
+
+        #fscore   yandex catboost - for categorial data
+
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', ])
+        self.model.fit(self.x, self.y_categorial, epochs=500, batch_size=16)
+        _, accuracy = self.model.evaluate(self.x, self.y_categorial)
+        print('Accuracy: %.2f' % (accuracy*100))
 
 
 
-dataset = pe.get_array(file_name = 'dataset.xls')
+    def serialize_model(self):
+        model_json = self.model.to_json()
+        with open("model.json", "w") as json_file:
+            json_file.write(model_json)
 
-new_list = []
-for i in dataset:
-    temp = []
-    for j in i:
-        byt = j.encode('utf-8')
-        to_int = int.from_bytes(byt, "big")
-        to_float = float.fromhex(hex(to_int))
-        temp.append(to_float)
-    new_list.append(temp)
-
-arr = numpy.asarray(new_list)
-
-x = arr[:, 0:11]
-xarr = x - x.mean()
-xarr = xarr / xarr.max()
+        # serialize weights to HDF5
+        self.model.save_weights("model.h5")
+        print("Saved model to disk")
 
 
-y_arr = numpy.asarray(dataset)
-y = y_arr[:, 11]
-encoder = LabelBinarizer()
-y_categorial = encoder.fit_transform(y)
-
-
-model = Sequential()
-model.add(Dense(11, input_shape=(11, ), activation='relu'))
-model.add(Dense(6, activation='relu'))
-model.add(Dense(7, activation='relu'))
-model.add(Dense(14, activation='relu'))
-
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(xarr, y_categorial, epochs=150, batch_size=10)
-
-
-_, accuracy = model.evaluate(xarr, y_categorial)
-print('Accuracy: %.2f' % (accuracy*100))
-
-
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
-print("Saved model to disk")
+if __name__ == ('__main__'):
+    t = Train()
+    t.set_x()
+    t.set_y()
+    t.train()
+    t.serialize_model()
